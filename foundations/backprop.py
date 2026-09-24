9af52668e012140e206e1adddc0fce75dadf7915
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-09-24 17:37:23 +0600

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -11,18 +11,17 @@ class Solution:
         # y_true: true target value
         #
         # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
-
+        # Loss: L = 0.5 * (y_hat - y_true)^2
+        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
         z=np.dot(x,w)+b
+        
         y_hat=1/(1+np.exp(-z))
 
-        # Loss: L = 0.5 * (y_hat - y_true)^2
+        loss= 0.5 * (y_hat-y_true)**2
 
-        l=0.5*(y_hat-y_true)**2
-
-        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
-
+        # y_hat = sigmoid ( xw + b )
         dl_dw=(y_hat-y_true)*(y_hat)*(1-y_hat)*x
-        dl_db=(y_hat-y_true)*y_hat*(1-y_hat)
+        dl_db=(y_hat-y_true)*(y_hat)*(1-y_hat)
 
-        return (np.round(dl_dw,5),np.round(dl_db,5))
-        
+        return np.round(dl_dw,5),np.round(dl_db,5)
+
