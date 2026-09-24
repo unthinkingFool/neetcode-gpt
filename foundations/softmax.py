4756a604062d1eb041795ed3a5dcae76e799ad1e
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-09-24 15:04:03 +0600

    Update Softmax function (Math Foundations 3/6)

--- foundations/softmax.py
+++ foundations/softmax.py
@@ -8,22 +8,18 @@ class Solution:
         # z is a 1D NumPy array of logits
         # Hint: subtract max(z) for numerical stability before computing exp
         # return np.round(your_answer, 4)
-        
-        """ why? --> there can be overflow for large values like : exp(1000) : so 
-         instead of taking the larger values we are just calculating with the 
-         relative values ( relative to the maximum value of z)
-        
-        """
-        arrNew=z-np.max(z)
+
+        max_element=np.max(z)
+
+        arrNew=z-max_element
 
         denom=0
         for i in range(z.shape[0]):
             denom=denom+np.exp(arrNew[i])
         
-        arr = np.zeros(z.shape[0])
-
+        arr=np.zeros(z.shape[0])
         for i in range(z.shape[0]):
             nom=np.exp(arrNew[i])
             arr[i]=nom/denom
 
-        return np.round(arr, 4)
+        return np.round(arr,4)
