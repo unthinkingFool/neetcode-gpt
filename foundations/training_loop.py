a9c7ccdebaad12aea1a0ff970d100bef61857113
Swapnil Das <swapnilpankajbeauty@gmail.com>
2026-09-26 17:36:52 +0600

    Update Training loop (Training 1/4)

--- foundations/training_loop.py
+++ foundations/training_loop.py
@@ -13,15 +13,20 @@ class Solution:
         # Model: y_hat = X @ w + b
         # Loss: MSE = (1/n) * sum((y_hat - y)^2)
         # Initialize w = zeros, b = 0
-        # return (np.round(w, 5), round(b, 5))\
+        # return (np.round(w, 5), round(b, 5))
+        num_features=X.shape[1]
+        num_samples=X.shape[0]
         b=0
-        w=np.zeros(X.shape[1])
+        w=np.zeros(num_features)
+
         for i in range(epochs):
-            y_hat=X@w+b
-            loss = np.mean((y_hat - y) ** 2)
-            dl_dw=2*(X.T@(y_hat-y))/X.shape[0]
-            dl_db = (2 / X.shape[0]) * np.sum(y_hat - y)
+            y_hat=X @ w + b
+            loss=np.mean((y_hat-y)**2)
+
+            dl_dw = ( 2*(y_hat-y) @ X)/num_samples
+            dl_db =  2 * np.sum(y_hat-y) / num_samples
+
             w=w-lr*dl_dw
             b=b-lr*dl_db
 
-        return (np.round(w,5),np.round(b,5))
+        return np.round(w,5), np.round(b,5)
